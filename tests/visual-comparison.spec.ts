97ecc52f42d60c4a8a8d956e6f9f50dc9589b376
Yevhen Yeshchev <yyeshchev@gmail.com>
2023-02-11 17:59:46 +0200

    updated workflow to save test-results

--- tests/visual-comparison.spec.ts
+++ tests/visual-comparison.spec.ts
@@ -1,12 +1,18 @@
 import { expect, test } from '@playwright/test';
 
+test.use({
+  viewport: {
+    width: 1280,
+    height: 720
+  }
+});
+
 test('Playwright banner matches with the snapshot', async ({ page }) => {
   // open base page
   await page.goto('https://playwright.dev/');
 
   // get header image and compare it with a golden screenshot
-  await expect(page.locator('css=.hero__title')).toHaveScreenshot(
-    'playwright-banner.png',
-    { maxDiffPixels: 100 }
+  expect(await page.locator('css=.hero__title').screenshot()).toMatchSnapshot(
+    'playwright-banner.png'
   );
 });
